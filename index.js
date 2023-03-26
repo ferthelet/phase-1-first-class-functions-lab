1b0e7f456d83c52998a11d442864ad09f395d03c
Juan Salazar <ferthelet@gmail.com>
2023-03-25 20:27:37 -0500

    adds first and last two drivers

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// js
+
+const returnFirstTwoDrivers = function(names) {
+    return names.length >= 2 ? names.slice(0, 2) : undefined;
+}
+
+const returnLastTwoDrivers = function(names) {
+    return names.length >= 2 ? names.slice(-2) : undefined;
+}
+
